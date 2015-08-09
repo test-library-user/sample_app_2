ecd018574afe6290a9e4436247de3e0c898ca0c1
Jordan Storz <jm.storz@gmail.com>
2015-08-09 12:50:51 -0600

    Edit email format validation to reject double-dot domains

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   before_save { downcase_email }
   
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
             
                                  private
   def downcase_email
-    self.email = email.downcase
+    self.email.downcase!
   end
   
 end
