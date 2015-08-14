6f40889af8fef68adada88bf2e0bcaf6956d6b55
wollvenMechaglade <jm.storz@gmail.com>
2015-08-14 09:25:30 -0600

    Add remember method to user.rb [8.4.1]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   
   before_save { downcase_email }
   
@@ -15,6 +16,16 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # Returns a random token.
+  def User.new_token
+    SecureRandom::urlsafe_base64
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
             
             
