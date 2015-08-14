7bcd83d63f210e57dcb34d93bd2f70f01c450a9d
wollvenMechaglade <jm.storz@gmail.com>
2015-08-14 09:51:09 -0600

    Add remember_digest column to user model / Add remember method and cookies [8.4.2]

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,15 @@ class User < ActiveRecord::Base
     SecureRandom::urlsafe_base64
   end
   
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
             
             
