cba7353ee57e5bb086147aa3db1abdce02a25f64
wollvenMechaglade <jm.storz@gmail.com>
2015-08-15 07:37:39 -0600

    Edit User::authenticated? to return false on blank argument [8.4.4]

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,12 +31,13 @@ class User < ActiveRecord::Base
   
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
   # Forgets a user.
   def forget
-    update_attributes(:remember_digest, nil)
+    update_attributes(remember_digest: nil)
   end
             
             
