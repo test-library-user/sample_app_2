ea9eb805016d9ecd126f7d20caee12e60efab58a
wollvenMechaglade <jm.storz@gmail.com>
2015-08-19 11:18:42 -0600

    Add controller tests for edit and update w/ authorization regarded. Add login checks in tests [9.2.1]

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = users(:jordan)
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
+  end
+  
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert flash.any?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.any?
+    assert_redirected_to login_url
   end
 
 end
